7f25e8547bf0a3f13abfed1df08fe95a308c1a00
AsimSafarli <asimseferli014@gmail.com>
2023-09-12 14:42:17 +0400

    add

---
+++ Screen/AllExpense.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function AllExpense() {
+  return (
+    <View>AllExpense</View>
+  )
+}
+
+export default AllExpense
---
+++ Screen/ManageExpense.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function ManageExpense() {
+  return (
+    <View>ManageExpense</View>
+  )
+}
+
+export default ManageExpense
